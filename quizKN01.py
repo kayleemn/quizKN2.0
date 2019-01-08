47ce9f0c1ebaa68f789b550e68cf182a7427a652
csPython <cspython@AD25TF04NGF1J-110.local>
2019-01-08 11:23:33 -0500

    2nd question added, score adjusted

--- quizKN01.py
+++ quizKN01.py
@@ -1,16 +1,24 @@
-#Questions.py - Kaylee
+#QuizKN.py - Kaylee
+#adding in 2nd question 
 
 #init variables
+score = int(0)
+
 q1 = """ Question 1: How old is Drake?
 1) 29
 2) 32
 3) 30
-4) 35
-"""
-
+4) 35"""
 a1= int(0)
 check1 = bool(False)
-score = int(0)
+
+q2 = """ Question 2: When is Drake's birthday?
+1) December 7th
+2) July 21st
+3) March 14th
+4) October 24th"""
+a2= int(0)
+check2 = bool(False)
 
 #intro
 print("Welcome to my question quiz")
@@ -37,7 +45,25 @@ while check1 == False:
             print("Please enter an integer between 0-5") #unacceptable integer
     except ValueError:
         print("Um.. let's think a little harder") # non-integer
-        
- #score
-print("You got a ", score * 100, "%")
+
+print(q2)
+
+while check2 == False:
+    try:
+        a2 = int(input("Choose the best reponse based on your knowledge on Drake.  "))
+        if a2 == 4:
+            print("Okay Cool!") #correct
+            score = int(score+1)
+            check1 = True
+        elif 0 < a2 < 5: #acceptable
+            print("Okay Cool!")
+            check2 = True
+        else:
+            print("Please enter an integer between 0-5") #unacceptable integer
+    except ValueError:
+        print("Um.. that's not one of the choices") # non-integer        
+
+
+#score
+print("You got a ", score * 50, "%")
 
