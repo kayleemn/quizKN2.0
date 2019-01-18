fb9bb0ee98602d3e9bd9399b683aca0b1f2b8333
csPython <cspython@AD25TF04NGF1J-110.local>
2019-01-18 09:34:35 -0500

    Added in functions to original code

--- quizKN01.py
+++ quizKN01.py
@@ -1,10 +1,31 @@
 #QuizKN.py - Kaylee
-from quizKNdata import *
-from quizKNfunc import *
-
 global grade
 grade = 0
-          
+
+#import file          
+from quizKNdata import *
+
+#define question/answer function
+    #name the function
+def run_qa_loop(qStr, qCheck, ansU, ansCor):
+    print(qStr)
+    
+    while qCheck == False:
+        try:
+            ansU = int(input("Choose the best reponse based on your knowledge on Drake.  "))
+            if ansU == ansCor:
+                print("Okay Cool!")
+                global grade
+                grade +=1
+                qCheck = True
+            elif 0 < ansU < 5:
+                print("Okay Cool!")
+                qCheck = True
+            else:
+                print("Please enter an integer between 0-5") #unacceptable integer
+        except ValueError:
+            print("Um.. that's not one of the choices") # non-integer
+
 #intro
 print("Welcome to my question quiz")
 print("In this progrm you'll be asked one question about my favorite artist!")
@@ -28,36 +49,29 @@ run_qa_loop(q9, a9, check9, q9Ans)
 run_qa_loop(q10, a10, check10, q10Ans)
 
 #score
-print("You got a ", grade * 10, "%")
+    print("You got a ", grade * 10, "%")
 
-if grade == 10: #100
-    print("Good job, you really know about Drake, we can be friends, 100%")
-elif grade == 9: #90
-    print("You almost got a 100%, 90%")
-elif grade == 8: #80
-    print("That's okay. 80%")
-elif grade == 7: #70
-    print("You're average, not the best. 70%")
-elif grade == 6: #60
-    print("That's not that special, 60%")
-elif grade == 5: #50
-    print("You only got half of those right, 50%")
-elif grade == 4: #40
-    print("Below a 50%? You do not know Drake, 40%")
-elif grade == 3: #30
-    print("I mean, not the greatest but, 30%")
-elif grade == 2: #20
-    print("That was the bare minimum, 20%")
-elif grade == 1: #10
-    print("You probably guessed for that one question, 10%")
-elif grade == 0: #0
-    print("You know nothing about Drake, kinda sad, we can't be friends, 0%")
-<<<<<<< HEAD
-=======
+    if grade == 10: #100
+        print("Good job, you really know about Drake, we can be friends, 100%")
+    elif grade == 9: #90
+        print("You almost got a 100%, 90%")
+    elif grade == 8: #80
+        print("That's okay. 80%")
+    elif grade == 7: #70
+        print("You're average, not the best. 70%")
+    elif grade == 6: #60
+        print("That's not that special, 60%")
+    elif grade == 5: #50
+        print("You only got half of those right, 50%")
+    elif grade == 4: #40
+        print("Below a 50%? You do not know Drake, 40%")
+    elif grade == 3: #30
+        print("I mean, not the greatest but, 30%")
+    elif grade == 2: #20
+        print("That was the bare minimum, 20%")
+    elif grade == 1: #10
+        print("You probably guessed for that one question, 10%")
+    elif grade == 0: #0
+        print("You know nothing about Drake, kinda sad, we can't be friends, 0%")
 
 
-
-
-    
-
->>>>>>> e3efe13b5d1444e5c96fbc5eef0570ae284179b6
